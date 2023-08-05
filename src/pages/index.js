fe64c4a9f5cd7241178ddeec8332a438d676831b
windsywinds <windsor.sam@gmail.com>
2023-08-05 18:23:08 +1200

    update home and loading for gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,13 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-9 ">
       <div className="flex w-screen">
-      <Image src={splash} alt="splash" className='absolute z-10 pointer-events-none' />
+      <Image src={splash} alt="splash" className=' pointer-events-none' />
       </div>
       
 
       <div className="flex flex-col z-30">
-      <p>Welcome, thanks for checking out my site, it's still under construction, so please check back later.</p>
-      <p className="pt-5">While I am setting up my web shop, if there are any prints you'd like, you can <Link href='components/contact' className="underline">send me a message here</Link>, <a className="underline" href="https://instagram.com/windsywinds/">or on Instagram @windsywinds</a>, and I'll get back to you with some prices and options!</p>
+      <p>Welcome, thanks for checking out my site, it's still under construction, but it's nearly ready!</p>
+      <p className="pt-5">While I am setting up my web shop, if there are any prints you'd like, you can <Link href='components/contact' className="underline">send me a message here</Link>, <a className="underline" href="https://instagram.com/windsywinds/">or on Instagram @windsywinds</a>, and I'll update the store or provide some options.</p>
       </div>
     </div>
   );
